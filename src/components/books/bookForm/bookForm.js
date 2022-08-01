cebf1fff1382f614e6d3fd5f1c6df39277a162ba
Harismehmoodulhaq <hsmeh.dev@gmail.com>
2022-08-01 12:02:02 +0300

    Set Loading bar in all component and error toast in book,student form
    remove unused imports

--- src/components/books/bookForm/bookForm.js
+++ src/components/books/bookForm/bookForm.js
@@ -1,11 +1,14 @@
 import ClearIcon from "@mui/icons-material/Clear";
 import ModeEditIcon from "@mui/icons-material/ModeEdit";
 import SaveIcon from "@mui/icons-material/Save";
+import MuiAlert from "@mui/material/Alert";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import IconButton from "@mui/material/IconButton";
+import LinearProgress from "@mui/material/LinearProgress";
+import Snackbar from "@mui/material/Snackbar";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import Toolbar from "@mui/material/Toolbar";
@@ -32,7 +35,7 @@ const bull = (
 
 export const BookForm = () => {
   const [update, setUpdate] = React.useState(false);
-  const { book } = useSelector(({ bookPage }) => bookPage);
+  const { book, isLoading, isError } = useSelector(({ bookPage }) => bookPage);
   const { patchBook } = useActionCreator();
   const [newBook, setBook] = React.useState({
     ...book,
@@ -40,131 +43,157 @@ export const BookForm = () => {
     expectedDateOfReturn: book ? new Date(book.expectedDateOfReturn) : "",
   });
 
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+
   const navigate = useNavigate();
   if (book === null) {
     return navigate("/books", { replace: true });
   }
   return (
-    <Box
-      sx={{
-        minWidth: 275,
-      }}
-    >
-      <Card variant="outlined">
-        <React.Fragment>
-          <CardContent>
-            <Box
-              sx={{
-                flexGrow: 1,
-              }}
-            >
-              <AppBar position="static">
-                <Toolbar
-                  sx={{
-                    backgroundColor: "#dee6f0",
-                    display: "flex",
-                    justifyContent: "flex-end",
-                  }}
-                  variant="dense"
-                >
-                  <Stack direction="row" spacing={1}>
-                    <IconButton
-                      onClick={() => {
-                        if (update) {
-                          patchBook(newBook);
-                        } else {
-                          setUpdate(!update);
-                        }
-                      }}
-                    >
-                      {update ? <SaveIcon /> : <ModeEditIcon />}
-                    </IconButton>
-                    <IconButton
-                      onClick={() => {
-                        navigate("/books", {
-                          replace: true,
+    <>
+      <div>
+        <Snackbar open={isError} autoHideDuration={1000}>
+          <Alert severity="error">Some Thing Went Wrong!</Alert>
+        </Snackbar>
+      </div>
+      <Box
+        sx={{
+          minWidth: 275,
+        }}
+      >
+        {isLoading ? <LinearProgress /> : <div></div>}
+        <Card variant="outlined">
+          <React.Fragment>
+            <CardContent>
+              <Box
+                sx={{
+                  flexGrow: 1,
+                }}
+              >
+                <AppBar position="static">
+                  <Toolbar
+                    sx={{
+                      backgroundColor: "#dee6f0",
+                      display: "flex",
+                      justifyContent: "flex-end",
+                    }}
+                    variant="dense"
+                  >
+                    <Stack direction="row" spacing={1}>
+                      <IconButton
+                        onClick={() => {
+                          if (update) {
+                            patchBook(newBook);
+                          } else {
+                            setUpdate(!update);
+                          }
+                        }}
+                      >
+                        {update ? <SaveIcon /> : <ModeEditIcon />}
+                      </IconButton>
+                      <IconButton
+                        onClick={() => {
+                          navigate("/books", {
+                            replace: true,
+                          });
+                        }}
+                      >
+                        <ClearIcon />
+                      </IconButton>
+                    </Stack>
+                  </Toolbar>
+                </AppBar>
+              </Box>
+              {/* Form */} <Toolbar />
+              <Box
+                component="form"
+                sx={{
+                  "& .MuiTextField-root": {
+                    m: 1,
+                    width: "55ch",
+                  },
+                }}
+                noValidate
+                autoComplete="off"
+              >
+                <div>
+                  <TextField
+                    label="Book Name"
+                    id="standard-size-normal"
+                    defaultValue={newBook.bookName}
+                    onChange={(evt) => {
+                      setBook({
+                        ...newBook,
+                        bookName: evt.target.value,
+                      });
+                    }}
+                    disabled={!update}
+                    variant="standard"
+                  />
+                  <TextField
+                    label="Author Name"
+                    id="standard-size-normal"
+                    defaultValue={newBook.authorName}
+                    onChange={(evt) => {
+                      setBook({
+                        ...newBook,
+                        authorName: evt.target.value,
+                      });
+                    }}
+                    disabled={!update}
+                    variant="standard"
+                  />
+                  <TextField
+                    label="Borrowed BY"
+                    id="standard-size-normal"
+                    defaultValue={newBook.borrowedBy}
+                    onChange={(evt) => {
+                      setBook({
+                        ...newBook,
+                        borrowedBy: evt.target.value,
+                      });
+                    }}
+                    disabled
+                    variant="standard"
+                  />
+                </div>
+                <div>
+                  <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <DateTimePicker
+                      label="BorrowAt"
+                      value={newBook.dateOfBorrow}
+                      disabled={!update}
+                      onChange={(value) => {
+                        setBook({
+                          ...newBook,
+                          dateOfBorrow: value,
                         });
                       }}
-                    >
-                      <ClearIcon />
-                    </IconButton>
-                  </Stack>
-                </Toolbar>
-              </AppBar>
-            </Box>
-            {/* Form */}
-            <Toolbar />
-            <Box
-              component="form"
-              sx={{
-                "& .MuiTextField-root": {
-                  m: 1,
-                  width: "55ch",
-                },
-              }}
-              noValidate
-              autoComplete="off"
-            >
-              <div>
-                <TextField
-                  label="Book Name"
-                  id="standard-size-normal"
-                  defaultValue={newBook.bookName}
-                  onChange={(evt) => {
-                    setBook({ ...newBook, bookName: evt.target.value });
-                  }}
-                  disabled={!update}
-                  variant="standard"
-                />
-                <TextField
-                  label="Author Name"
-                  id="standard-size-normal"
-                  defaultValue={newBook.authorName}
-                  onChange={(evt) => {
-                    setBook({ ...newBook, authorName: evt.target.value });
-                  }}
-                  disabled={!update}
-                  variant="standard"
-                />
-                <TextField
-                  label="Borrowed BY"
-                  id="standard-size-normal"
-                  defaultValue={newBook.borrowedBy}
-                  onChange={(evt) => {
-                    setBook({ ...newBook, borrowedBy: evt.target.value });
-                  }}
-                  disabled={!update}
-                  variant="standard"
-                />
-              </div>
-              <div>
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                  <DateTimePicker
-                    label="BorrowAt"
-                    value={newBook.dateOfBorrow}
-                    disabled={!update}
-                    onChange={(value) => {
-                      setBook({ ...newBook, dateOfBorrow: value });
-                    }}
-                    renderInput={(params) => <TextField {...params} />}
-                  />
-                </LocalizationProvider>
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                  <DateTimePicker
-                    label="ReturnAt"
-                    value={newBook.expectedDateOfReturn}
-                    disabled={!update}
-                    onChange={(value) => {
-                      setBook({ ...newBook, expectedDateOfReturn: value });
-                    }}
-                    renderInput={(params) => <TextField {...params} />}
-                  />
-                </LocalizationProvider>
-              </div>
-            </Box>
-          </CardContent>
-        </React.Fragment>
-      </Card>
-    </Box>
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </LocalizationProvider>
+                  <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <DateTimePicker
+                      label="ReturnAt"
+                      value={newBook.expectedDateOfReturn}
+                      disabled={!update}
+                      onChange={(value) => {
+                        setBook({
+                          ...newBook,
+                          expectedDateOfReturn: value,
+                        });
+                      }}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </LocalizationProvider>
+                </div>
+              </Box>
+            </CardContent>
+          </React.Fragment>
+        </Card>
+      </Box>
+    </>
   );
 };
--- src/components/students/studentForm/studentForm.js
+++ src/components/students/studentForm/studentForm.js
@@ -1,17 +1,17 @@
 import ClearIcon from "@mui/icons-material/Clear";
 import ModeEditIcon from "@mui/icons-material/ModeEdit";
 import SaveIcon from "@mui/icons-material/Save";
+import MuiAlert from "@mui/material/Alert";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import IconButton from "@mui/material/IconButton";
+import LinearProgress from "@mui/material/LinearProgress";
+import Snackbar from "@mui/material/Snackbar";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import Toolbar from "@mui/material/Toolbar";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import * as React from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -32,7 +32,9 @@ const bull = (
 
 export const StudentForm = () => {
   const [update, setUpdate] = React.useState(false);
-  const { student } = useSelector(({ studentPage }) => studentPage);
+  const { student, isLoading, isError } = useSelector(
+    ({ studentPage }) => studentPage
+  );
   const { patchStudent } = useActionCreator();
   const [newStudent, setStudent] = React.useState({
     ...student,
@@ -40,95 +42,112 @@ export const StudentForm = () => {
 
   const navigate = useNavigate();
   if (student === null) {
+    debugger;
     return navigate("/students", { replace: true });
   }
+
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+
   return (
-    <Box
-      sx={{
-        minWidth: 275,
-      }}
-    >
-      <Card variant="outlined">
-        <React.Fragment>
-          <CardContent>
-            <Box
-              sx={{
-                flexGrow: 1,
-              }}
-            >
-              <AppBar position="static">
-                <Toolbar
-                  sx={{
-                    backgroundColor: "#dee6f0",
-                    display: "flex",
-                    justifyContent: "flex-end",
-                  }}
-                  variant="dense"
-                >
-                  <Stack direction="row" spacing={1}>
-                    <IconButton
-                      onClick={() => {
-                        if (update) {
-                          patchStudent(newStudent);
-                        } else {
-                          setUpdate(!update);
-                        }
-                      }}
-                    >
-                      {update ? <SaveIcon /> : <ModeEditIcon />}
-                    </IconButton>
-                    <IconButton
-                      onClick={() => {
-                        navigate("/students", {
-                          replace: true,
-                        });
-                      }}
-                    >
-                      <ClearIcon />
-                    </IconButton>
-                  </Stack>
-                </Toolbar>
-              </AppBar>
-            </Box>
-            {/* Form */}
-            <Toolbar />
-            <Box
-              component="form"
-              sx={{
-                "& .MuiTextField-root": {
-                  m: 1,
-                  width: "87ch",
-                },
-              }}
-              noValidate
-              autoComplete="off"
-            >
-              <div>
-                <TextField
-                  label="FirstName"
-                  id="standard-size-normal"
-                  defaultValue={newStudent.firstName}
-                  onChange={(evt) => {
-                    setStudent({ ...newStudent, firstName: evt.target.value });
-                  }}
-                  disabled={!update}
-                  variant="standard"
-                />
-                <TextField
-                  label="LastName"
-                  id="standard-size-normal"
-                  defaultValue={newStudent.lastName}
-                  onChange={(evt) => {
-                    setStudent({ ...newStudent, lastName: evt.target.value });
-                  }}
-                  disabled={!update}
-                  variant="standard"
-                />
-              </div>
-            </Box>
-          </CardContent>
-        </React.Fragment>
-      </Card>
-    </Box>
+    <>
+      <div>
+        <Snackbar open={isError} autoHideDuration={1000}>
+          <Alert severity="error">Some Thing Went Wrong!</Alert>
+        </Snackbar>
+      </div>
+      <Box
+        sx={{
+          minWidth: 275,
+        }}
+      >
+        {isLoading ? <LinearProgress /> : <div></div>}
+        <Card variant="outlined">
+          <React.Fragment>
+            <CardContent>
+              <Box
+                sx={{
+                  flexGrow: 1,
+                }}
+              >
+                <AppBar position="static">
+                  <Toolbar
+                    sx={{
+                      backgroundColor: "#dee6f0",
+                      display: "flex",
+                      justifyContent: "flex-end",
+                    }}
+                    variant="dense"
+                  >
+                    <Stack direction="row" spacing={1}>
+                      <IconButton
+                        onClick={() => {
+                          if (update) {
+                            patchStudent(newStudent);
+                          } else {
+                            setUpdate(!update);
+                          }
+                        }}
+                      >
+                        {update ? <SaveIcon /> : <ModeEditIcon />}
+                      </IconButton>
+                      <IconButton
+                        onClick={() => {
+                          navigate("/students", {
+                            replace: true,
+                          });
+                        }}
+                      >
+                        <ClearIcon />
+                      </IconButton>
+                    </Stack>
+                  </Toolbar>
+                </AppBar>
+              </Box>
+              {/* Form */}
+              <Toolbar />
+              <Box
+                component="form"
+                sx={{
+                  "& .MuiTextField-root": {
+                    m: 1,
+                    width: "87ch",
+                  },
+                }}
+                noValidate
+                autoComplete="off"
+              >
+                <div>
+                  <TextField
+                    label="FirstName"
+                    id="standard-size-normal"
+                    defaultValue={newStudent.firstName}
+                    onChange={(evt) => {
+                      setStudent({
+                        ...newStudent,
+                        firstName: evt.target.value,
+                      });
+                    }}
+                    disabled={!update}
+                    variant="standard"
+                  />
+                  <TextField
+                    label="LastName"
+                    id="standard-size-normal"
+                    defaultValue={newStudent.lastName}
+                    onChange={(evt) => {
+                      setStudent({ ...newStudent, lastName: evt.target.value });
+                    }}
+                    disabled={!update}
+                    variant="standard"
+                  />
+                </div>
+              </Box>
+            </CardContent>
+          </React.Fragment>
+        </Card>
+      </Box>
+    </>
   );
 };
